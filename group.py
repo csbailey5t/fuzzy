74032599861e0216ec9ff74ddd4d4aeb3d047c90
Scott Bailey <csb5t@virginia.edu>
2016-05-19 14:18:11 -0400

    Add some print statements to track progress

--- group.py
+++ group.py
@@ -9,18 +9,22 @@ data.buyer = data.buyer.fillna('no_buyer')
 data.dest = data.dest.fillna('no_dest')
 
 dests = []
-threshold = 70
+threshold = 90
 
 for dest in data.dest:
     if dest not in dests:
         dests.append(dest)
+
+print("dests", dests)
 
 grouped = data.groupby(['dest'])
 
 with open('filtered.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerow(['dest', 'buyer', 'group_id'])
+    count = 0
     for (name, group) in grouped:
+        # here to 36 - abstract into function
         deduped = []
         buyers = set(group.buyer.tolist())
         while buyers:
@@ -33,9 +37,16 @@ with open('filtered.csv', 'w') as f:
             filtered.add(current_buyer)
             deduped.append(filtered)
             buyers -= filtered
-
+        print("while loop has ended")
+        print("buyers", buyers)
+        # for duduped in alldedupeds:
         for (group_id, dups) in enumerate(deduped):
             writer.writerows(
                 (name, buyer, name + str(group_id))
                 for buyer in dups
             )
+        count += 1
+        print("count", count)
+        print("write row for loop has ended")
+
+    print('for loop for name group has ended')
