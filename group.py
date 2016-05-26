a9a5e07843b9c06773b637b4d9dfc9414299c67a
Scott Bailey <csb5t@virginia.edu>
2016-05-26 13:32:25 -0400

    parallelize

--- group.py
+++ group.py
@@ -1,7 +1,7 @@
-from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
-import pandas as pd
 import csv
+from fuzzywuzzy import fuzz, process
+from multiprocessing import Pool
+import pandas as pd
 
 data = pd.read_csv('destxbuyer.csv', encoding='latin-1')
 
@@ -19,34 +19,34 @@ print("dests", dests)
 
 grouped = data.groupby(['dest'])
 
+
+def find_dupes(name_group):
+    name, group = name_group
+    deduped = []
+    buyers = set(group.buyer.tolist())
+    while buyers:
+        current_buyer = buyers.pop()
+        dupes = process.extract(
+                current_buyer, buyers,
+                scorer=fuzz.token_set_ratio
+                )
+        filtered = set(x[0] for x in dupes if x[1] > threshold)
+        if len(filtered) > 0:
+            filtered.add(current_buyer)
+        deduped.append(filtered)
+        buyers -= filtered
+    return (name, deduped)
+
 with open('filtered.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerow(['dest', 'buyer', 'group_id'])
-    count = 0
-    for (name, group) in grouped:
-        # here to 36 - abstract into function
-        deduped = []
-        buyers = set(group.buyer.tolist())
-        while buyers:
-            current_buyer = buyers.pop()
-            dupes = process.extract(
-                    current_buyer, buyers,
-                    scorer=fuzz.token_set_ratio
-                    )
-            filtered = set(x for x in dupes if x[1] > threshold)
-            filtered.add(current_buyer)
-            deduped.append(filtered)
-            buyers -= filtered
-        print("while loop has ended")
-        print("buyers", buyers)
-        # for duduped in alldedupeds:
-        for (group_id, dups) in enumerate(deduped):
+
+    with Pool() as pool:
+        deduped_groups = pool.map(find_dupes, grouped)
+
+    for (name, group) in deduped_groups:
+        for (group_id, dups) in enumerate(group):
             writer.writerows(
                 (name, buyer, name + str(group_id))
                 for buyer in dups
             )
-        count += 1
-        print("count", count)
-        print("write row for loop has ended")
-
-    print('for loop for name group has ended')
